a4d6f315340dd5b05f9f74f171db4deec298d338
sordonez1996 <so235@njit.edu>
2020-10-28 17:57:56 -0400

    Task: Import

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -10,16 +10,17 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator( self ):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_results_addition_calculator(self):
+    def test_results_addition_calculator( self ):
         test_data = CsvReader('/src/Addition.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
-    def test_results_subtraction_calculator(self):
-        test_data = CsvReader('/src/Subtraction.csv').data
+
+    def test_multiplication_method_calculator( self ):
+        test_data = CsvReader('/src/Multiplication.csv').data
         for row in test_data:
-            self.assertEqual(self.calculator.subtract(row['Value 2'], row['Value 1']), int(row['Result']))
+            self.assertEqual(self.calculator.multiplication(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
 

160041db803b0baf2e134220db81656c5f1bb2d5
sordonez1996 <so235@njit.edu>
2020-10-28 17:23:09 -0400

    Task: Import

---
+++ src/calculatortests.py
@@ -0,0 +1,20 @@
+import unittest
+from CsvReader import CsvReader
+from Calculator import Calculator
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+
+    def test_results_addition_calculator(self):
+        test_data = CsvReader('/src/Addition.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+if __name__ == '__main__':
+    unittest.main()

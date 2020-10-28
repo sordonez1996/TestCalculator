7b4f9f8f8ed248ad3094e8b6d81386061cfed3ac
sordonez1996 <so235@njit.edu>
2020-10-28 17:38:15 -0400

    Task: Import

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -2,19 +2,26 @@ import unittest
 from CsvReader import CsvReader
 from Calculator import Calculator
 
+
 class MyTestCase(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp( self ) -> None:
         self.calculator = Calculator()
 
-    def test_instantiate_calculator(self):
+    def test_instantiate_calculator( self ):
         self.assertIsInstance(self.calculator, Calculator)
-
 
     def test_results_addition_calculator(self):
         test_data = CsvReader('/src/Addition.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_results_subtraction_calculator( self ):
+        test_data = CsvReader('/src/Subtraction.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(row['Value 2'], row['Value 1']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
 
 if __name__ == '__main__':
     unittest.main()

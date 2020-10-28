8219c161148b2479ccd08d1e6391428fbd2ec858
sordonez1996 <so235@njit.edu>
2020-10-28 18:50:06 -0400

    Task: Import

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,28 +1,28 @@
 import unittest
 from CsvReader import CsvReader
 from Calculator import Calculator
-
+from pprint import pprint
 
 class MyTestCase(unittest.TestCase):
     def setUp( self ) -> None:
         self.calculator = Calculator()
 
-    def test_instantiate_calculator( self ):
+    def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_results_addition_calculator( self ):
+    def test_results_addition_calculator(self):
         test_data = CsvReader('/src/Addition.csv').data
+        pprint(test_data)
         for row in test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
-
-    def test_multiplication_method_calculator( self ):
-        test_data = CsvReader('/src/Multiplication.csv').data
+    def test_results_subtraction_calculator( self ):
+        test_data = CsvReader('/src/Subtraction.csv').data
+        pprint(test_data)
         for row in test_data:
-            self.assertEqual(self.calculator.multiplication(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.subtract(row['Value 2'], row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-
 
 if __name__ == '__main__':
     unittest.main()
--- src/CsvReader.py
+++ src/CsvReader.py
@@ -8,15 +8,10 @@ class CsvReader:
     data = []
 
     def __init__(self, filepath):
+        self.data = []
+
         with open(filepath) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
         pass
-
-    '''def return_data_as_objects(self, class_name):
-        objects = []
-        for row in self.data:
-            objects.append(ClassFactory(class_name, row))
-        return objects
-    '''
